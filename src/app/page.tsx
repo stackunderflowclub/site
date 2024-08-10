7215dde6d498735fd9baca0b47b80ecfb19ce29b
akpi816218 <gh@akpi.is-a.dev>
2024-08-09 17:44:17 -0700

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<main className='p-8 py-16 pt-32 bg-5 h-[75vh] text-center text-white flex flex-col justify-center items-center gap-2 md:gap-4 lg:gap-8'>
+				<h1 className='font-black text-4xl '>Welcome to Stack Underflow Club</h1>
+				<p className='text-2xl md:text-3xl p-8 md:px-16 lg:px-32 xl:px-64'>
+					Hi there! I'm a young developer with primary experience in web design
+					and development.
+				</p>
+			</main>
+		</>
+	);
 }
